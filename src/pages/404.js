8975bb59fe1326db7f0c3f7b4605b4bfd8fb51c6
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-05-03 23:22:54 +0530

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,31 @@
-import { useEffect } from 'react';
-import { navigate } from 'gatsby';
+import React, { Component } from 'react'
+import { Top } from '../components/top'
+import { Header } from '../components/header'
+import { ThemeSwitch } from '../components/theme-switch'
+import { rhythm } from '../utils/typography'
+import { Link } from 'gatsby'
 
-export default () => {
-  useEffect(() => {
-    navigate('/');
-  }, []);
-  return null;
-};
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Top title='Red Quark' location='/' rootPath='//' />
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(32),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <ThemeSwitch />
+          <Header title='Red Quark' location='' rootPath='/' />
+          <div style={{ textAlign: "center" }}>
+            <h1>Page Not Found &#129301;. Go to <Link to="/">home</Link></h1>
+          </div>
+        </div>
+      </React.Fragment>)
+  }
+}
+
+export default NotFoundPage
